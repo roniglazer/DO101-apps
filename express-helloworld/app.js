966db63bb588b8c005401d274955b41b2391c809
roni glazer <roniglaz@gmail.com>
2022-07-14 15:34:10 +0300

    num

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,13 +4,15 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
-
+console.log(1);
 app.get('/mars', function(req, res) { 
   res.send('Hello Mars!\n'); 
 });
 app.get('/roni', function(req, res) { 
   res.send('Hello roni!\n'); 
 });
+console.log(2);
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
